09a07ad7ecbf971f8509f2de921ebbf298c63242
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-06-20 18:38:29 +0200

    Move title

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,19 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>
+          Scoreboarder: Keep track of scoreboards on Discord with ease
+        </title>
+        <meta
+          name="description"
+          content="Scoreboarder is a Discord bot for making scoreboards (or leaderboards). You can create scoreboards and add or remove points from people and much more!"
+        />
+        <meta property="og:title" content="Scoreboarder"></meta>
+        <meta
+          property="og:description"
+          content="Keep track of scoreboards on Discord with ease"
+        ></meta>
+
         {/* Next.js complains if this isn't here */}
         <meta
           name="viewport"
