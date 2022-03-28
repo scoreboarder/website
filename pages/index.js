5cba0f11205b6d949729fd7cc3e6e4b7cf52387b
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-03-29 00:52:14 +0200

    Increase to 30min

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export async function getStaticProps() {
       issues: await fetchGithubIssues(),
       currentServers: await fetchTopGGStats(),
     },
-    revalidate: 60 * 10,
+    revalidate: 60 * 30,
   };
 }
 
@@ -79,52 +79,3 @@ async function fetchTopGGStats() {
 
   return json?.server_count ?? "Error";
 }
-
-// export async function getServerSideProps(context) {
-//   const truncateString = (str, num) => {
-//     if (str.length > num) {
-//       return str.slice(0, num) + "...";
-//     } else {
-//       return str;
-//     }
-//   };
-
-//   const topggStatsResponse = await fetch(
-//     "https://top.gg/api/bots/641229153433288724",
-//     {
-//       method: "GET",
-//       headers: {
-//         Accept: "application/json",
-//         Authorization: process.env.TOPGG_TOKEN,
-//       },
-//     }
-//   );
-
-//   const topggStats = await topggStatsResponse.json();
-
-//   const issuesResponse = await fetch(
-//     "https://api.github.com/repos/scoreboarder/suggestions/issues",
-//     {
-//       method: "GET",
-//       headers: { Accept: "application/vnd.github.v3+json" },
-//     }
-//   );
-
-//   const issues = (await issuesResponse.json())
-//     .filter((item) => {
-//       return item.labels.some((label) => label.name == "accepted");
-//     })
-//     .map((item) => ({
-//       number: item.number,
-//       title: item.title,
-//       body: truncateString(item.body, 100),
-//       url: item.html_url,
-//     }));
-
-//   return {
-//     props: {
-//       issues,
-//       currentServers: topggStats?.server_count,
-//     },
-//   };
-// }
