721c4dcba50d2e2fa1d03a27d92c6e826ea9dcd5
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-11-01 19:54:47 +0100

    Forgot an import

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { withPlausibleProxy } = require("next-plausible");
+
 module.exports = withPlausibleProxy()({
   // ...your next js config, if any
   // Important! it is mandatory to pass a config object, even if empty
